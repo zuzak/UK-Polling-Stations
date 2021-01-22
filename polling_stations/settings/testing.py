cf6d00cbbce08bb370cbaab199042bb812fc0c31
GeoWill <roper.william@gmail.com>
2021-01-22 10:28:03 +0000

    DB to testing

--- polling_stations/settings/testing.py
+++ polling_stations/settings/testing.py
@@ -1,5 +1,15 @@
 from .base import *  # noqa
 
+DATABASES = {
+    "default": {
+        "ENGINE": "django.contrib.gis.db.backends.postgis",
+        "NAME": "test",
+        "USER": "postgres",
+        "PASSWORD": "",
+        "HOST": "localhost",
+        "PORT": "",
+    }
+}
 EVERY_ELECTION["CHECK"] = True  # noqa
 NEXT_CHARISMATIC_ELECTION_DATE = None
 DISABLE_GA = True  # don't log to Google Analytics when we are running tests
